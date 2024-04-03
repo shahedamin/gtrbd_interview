3523f651e375069246a09efc78f293bb1a50e441
Shahed <shahed.amin456@gmail.com>
2024-04-03 16:52:59 +0600

    update(can search data within a date frame)

--- assignment.py
+++ assignment.py
@@ -1,21 +1,18 @@
 
 import yfinance as yf
 
-
 class Trading_strategy:
-    # def __init__(self,symbol,from_date,to_date):
-    def __init__(self,symbol):
+    def __init__(self,symbol,from_date,to_date):
        self.company_name = symbol
-    #    self.from_date = from_date
-    #    self.to_date = to_date
+       self.from_date = from_date
+       self.to_date = to_date
 
     def data_acquisition(self):
-        aapl = yf.Ticker(self.company_name)
 
-        # get historical market data
-        hist = aapl.history(start="2024-01-04", end=None)
+        aapl = yf.Ticker(self.company_name)
+        hist = aapl.history(start=self.from_date, end= self.to_date) # for see the data till today, use end = None
         print(hist)
 
       
-aapl_strategy=Trading_strategy("AAPL")
+aapl_strategy=Trading_strategy("AAPL","2024-02-01","2024-04-01")
 aapl_strategy.data_acquisition()
