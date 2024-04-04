49594ef3f144051067a2c46460fff4ddb5e416bf
Shahed <shahed.amin456@gmail.com>
2024-04-04 07:21:52 +0600

    func(clean data method added)

--- assignment.py
+++ assignment.py
@@ -7,12 +7,19 @@ class Trading_strategy:
        self.from_date = from_date
        self.to_date = to_date
 
+    #Get data
     def data_acquisition(self):
-
         aapl = yf.Ticker(self.company_name)
-        hist = aapl.history(start=self.from_date, end= self.to_date) # for see the data till today, use end = None
-        print(hist)
+        self.hist = aapl.history(start=self.from_date, end= self.to_date) # for see the data till today, use end = None
+
+    # clean data
+    def clean_data(self):
+        self.hist = self.hist.drop_duplicates() # remove duplicates
+        self.hist = self.hist.ffill() # forward fill NaN values
 
       
-aapl_strategy=Trading_strategy("AAPL","2024-02-01","2024-04-01")
-aapl_strategy.data_acquisition()
+aapl_strategy=Trading_strategy("AAPL","2024-02-01",None)
+aapl_strategy. data_acquisition()
+aapl_strategy.clean_data()
+print (aapl_strategy.hist)
+
